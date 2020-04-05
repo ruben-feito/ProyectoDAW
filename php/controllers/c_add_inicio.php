1c1f8fedbf33dc27057e6362ebd046e8f4efafb2
ruben-feito <59911220+ruben-feito@users.noreply.github.com>
2020-04-05 17:57:10 +0200

    Add files via upload

--- php/controllers/c_add_inicio.php
+++ php/controllers/c_add_inicio.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>admin-inicio</title>
-    <link rel="stylesheet" href="../.././js/bootstrap.min.css">
+    <link rel="stylesheet" href="../.././css/bootstrap.min.css">
     <link rel="stylesheet" href="../.././css/stylesheet-admin.css">
     </head>
    
