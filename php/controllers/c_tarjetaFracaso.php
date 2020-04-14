0f76da12dcafc6bc624cacd0dc0281f3ab91041d
ruben-feito <59911220+ruben-feito@users.noreply.github.com>
2020-04-14 18:01:37 +0200

    Add files via upload

--- php/controllers/c_tarjetaFracaso.php
+++ php/controllers/c_tarjetaFracaso.php
@@ -12,7 +12,7 @@
 		<title>El Rinc&oacute;n del "Sin"</title>
 		<meta charset="UTF-8"/>
 		<meta name="author" content="Rubén Feito Alberto Jimenez y Jesús Mateos">
-		<link rel="icon" href="./img/logo2.png"/> <!--Fab Icon -->
+		<link rel="icon" href="../../img/logo2.png"/> <!--Fab Icon -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viweport-->
 		<link rel="StyleSheet" media="only screen and (max-device-width:760px)" href="../../css/stylesheet-movil.css" type="text/css">
 		<link rel="StyleSheet" media="only screen and (min-device-width:761px) and (max-device-width:1024px)" href="../../css/stylesheet-tablet.css" type="text/css"/>
