ae96772c17f2d37b86bdfdead819316bc5b9135b
ruben-feito <59911220+ruben-feito@users.noreply.github.com>
2020-04-15 20:54:06 +0200

    Add files via upload

--- php/views/v_add_pedidos.php
+++ php/views/v_add_pedidos.php
@@ -5,14 +5,14 @@ if(count($pedidos)==0){
 }
 else{
     echo "<table>
-    <tr><th>Nº Pedido</th><th>Email</th><th>Metodo Pago</th><th>Tarjeta</th><th>Fecha Registro</th></tr>";
+    <tr><th>Nº Pedido</th><th>Email</th><th>Metodo Pago</th><th>Total €</th><th>Fecha Registro</th></tr>";
     foreach ($pedidos as $pedido) {
 
         echo "
         <tr><td width='200'>".$pedido['num_pedido']."
         </td><td width='200'>".$pedido['email']."
         </td><td width='200'>".$pedido['metodo_pago']."
-        </td><td width='200'>".$pedido['tarjeta']."
+        </td><td width='200'>".$pedido['total']."
         </td><td width='200'>".$pedido['fecha_registro']."
         </td></tr>";
     }
