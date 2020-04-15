81a1c5523d7c1534eb5e59222f8591ac36f337cf
ruben-feito <59911220+ruben-feito@users.noreply.github.com>
2020-04-15 21:39:23 +0200

    Add files via upload

--- php/models/m_add_pedidos_fechas.php
+++ php/models/m_add_pedidos_fechas.php
@@ -14,21 +14,21 @@ $fechaHasta=date("Y-m-d", $fechaHasta);
 $pedidos = array();
 
 //Seleccionar los pedidos
-$sql = "SELECT num_pedido, email, metodo_pago, tarjeta, fecha_registro FROM pedido where DATE_FORMAT(fecha_registro,'%Y-%m-%d')>='$fechaDesde' AND DATE_FORMAT(fecha_registro,'%Y-%m-%d')<='$fechaHasta' order by num_pedido";
+$sql = "SELECT num_pedido, email, metodo_pago, total, fecha_registro FROM pedido where DATE_FORMAT(fecha_registro,'%Y-%m-%d')>='$fechaDesde' AND DATE_FORMAT(fecha_registro,'%Y-%m-%d')<='$fechaHasta' order by num_pedido";
 $resultado = mysqli_query($conn, $sql);
 if ($resultado) {
     while ($row = mysqli_fetch_assoc($resultado)) {
         $num_pedido=$row['num_pedido'];
         $email=$row['email'];
         $metodo_pago=$row['metodo_pago'];
-        $tarjeta=$row['tarjeta'];
+        $total=$row['total'];
         $fecha_registro=$row['fecha_registro'];
 
         $pedidos[]=[
             "num_pedido"=>$num_pedido,
             "email"=>$email, 
             "metodo_pago"=>$metodo_pago,  
-            "tarjeta"=>$tarjeta,
+            "total"=>$total,
             "fecha_registro"=>$fecha_registro];
     }
 }
