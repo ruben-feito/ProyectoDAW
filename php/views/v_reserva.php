52cfc4ed2fa0c78de43bbabe7467123bd33bcd3d
ruben-feito <59911220+ruben-feito@users.noreply.github.com>
2020-04-25 18:20:42 +0200

    Delete v_reserva.php

--- php/views/v_reserva.php
+++
@@ -1,4 +0,0 @@
-<br><br>
-<p>Reserva Realizada Correctamente.</p>
-<p>Enviado correo de confirmacion a <?php echo $email ?></p>
-<!-- <input id="disp" type="button" name="atras" value="Atras" onclick="history.go(-2)">  -->
